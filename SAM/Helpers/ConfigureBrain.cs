57b37d8a46ba1ec73ef1eecc14730b899fb2f5af
samuel grahame <samuel@intelogy.com.au>
2018-07-19 17:01:11 +1000

    Add project files.

---
+++ SAM/Helpers/ConfigureBrain.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SAM.Helpers
+{
+    public class ConfigureBrain
+    {
+        public string BrainModules;
+
+        public void Think()
+        {
+
+        }
+
+        public ConfigureBrain Store()
+        {
+            if (Directory.Exists("Brain"))
+                Directory.CreateDirectory("Brain");
+            if (File.Exists("Brain/Module.brain"))
+                File.WriteAllText("Brain/Module.brain", BrainModules);
+            return this;
+        }
+
+        public ConfigureBrain Init()
+        {
+            BrainModules = string.Empty;
+
+            if (Directory.Exists("Brain"))
+                Directory.CreateDirectory("Brain");
+            if(File.Exists("Brain/Module.brain"))
+                BrainModules = File.ReadAllText("Brain/Module.brain");
+
+            return this;
+        }
+    }
+}
---
+++ SAM/Program.cs
@@ -0,0 +1,45 @@
+﻿using SAM.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SAM
+{
+    class Program
+    {
+        [ThreadStatic]
+        static ConfigureBrain ActiveBrain;
+        static void Main(string[] args)
+        {
+            ActiveBrain = new ConfigureBrain().Init();
+
+            // while alive
+            try
+            {
+                var thread = new Thread(() =>
+                {
+                    ActiveBrain.Think();
+
+                });
+                thread.Start();    
+                
+                while(thread.IsAlive)
+                {
+
+                }
+
+                // lived to old age
+            }
+            catch (Exception)
+            {
+                // got murdered!
+
+            }
+
+            ActiveBrain.Store();
+        }
+    }
+}
